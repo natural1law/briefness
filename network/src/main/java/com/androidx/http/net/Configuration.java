370e48d973969933ed4c7ad5c88addd950127b93
natural1law <17640051884@163.com>
2021-07-07 09:44:36 +0800

    bug修复

--- network/src/main/java/com/androidx/http/net/Configuration.java
+++ network/src/main/java/com/androidx/http/net/Configuration.java
@@ -2,13 +2,14 @@ package com.androidx.http.net;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class Configuration implements Serializable {
 
     private Configuration() {
     }
 
-    private static Map<String, String> headers;
+    private static Map<String, String> headers = new ConcurrentHashMap<>();
 
 
     public static Map<String, String> getHeaders() {
