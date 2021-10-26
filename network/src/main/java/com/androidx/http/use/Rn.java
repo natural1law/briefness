47b4f64804970b651bec369dccb5c5bc5429b9fa
natural1law <17640051884@163.com>
2021-10-26 17:24:49 +0800

    ...

--- network/src/main/java/com/androidx/http/use/Rn.java
+++ network/src/main/java/com/androidx/http/use/Rn.java
@@ -65,7 +65,6 @@ public final class Rn {
                 .setDisconnect(disconnect)
                 .setException(exception)
                 .build());
-        Executors.newFixedThreadPool(10);
     }
 
     /**
