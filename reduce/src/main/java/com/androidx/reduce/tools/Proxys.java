68cec09679c32cdac8b29e50994fa930dae149d9
natural1law <17640051884@163.com>
2022-05-26 14:34:54 +0800

    1.修复已知问题

--- reduce/src/main/java/com/androidx/reduce/tools/Proxys.java
+++ reduce/src/main/java/com/androidx/reduce/tools/Proxys.java
@@ -15,8 +15,8 @@ public final class Proxys {
      * @param t 实例化对象
      */
     public static <T> T getInstance(T t) {
-        var loader = t.getClass().getClassLoader();
-        var inter = t.getClass().getInterfaces();
+        ClassLoader loader = t.getClass().getClassLoader();
+        Class<?>[] inter = t.getClass().getInterfaces();
         return (T) Proxy.newProxyInstance(loader, inter, (proxy, method, args) -> method.invoke(t, args));
     }
 
@@ -27,7 +27,7 @@ public final class Proxys {
      */
     public static <T> T getInstance(Class<T> t) {
         try {
-            var clazz = Class.forName(Objects.requireNonNull(t.getCanonicalName()));
+            Class<?> clazz = Class.forName(Objects.requireNonNull(t.getCanonicalName()));
             T instance = (T) newInstance(clazz);
             synchronized (Class.class) {
                 return instance;
@@ -42,9 +42,9 @@ public final class Proxys {
      * 根据反射实例化代理对象
      */
     private static <T> Object newInstance(Class<T> clazz) throws Exception {
-        var newInstance = (T) clazz.getDeclaredConstructor().newInstance();
-        var loader = newInstance.getClass().getClassLoader();
-        var inter = newInstance.getClass().getInterfaces();
+        T newInstance = clazz.getDeclaredConstructor().newInstance();
+        ClassLoader loader = newInstance.getClass().getClassLoader();
+        Class<?>[] inter = newInstance.getClass().getInterfaces();
         return Proxy.newProxyInstance(loader, inter, (proxy, method, args) -> method.invoke(newInstance, args));
     }
 
