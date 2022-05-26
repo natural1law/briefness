518e2e1d22bc24b5c0a0d1eadbb042585dbfe9e3
natural1law <17640051884@163.com>
2022-05-26 14:08:47 +0800

    1.修复已知问题

--- reduce/src/main/java/com/androidx/reduce/tools/Proxys.java
+++ reduce/src/main/java/com/androidx/reduce/tools/Proxys.java
@@ -3,9 +3,8 @@ package com.androidx.reduce.tools;
 import java.lang.reflect.Proxy;
 import java.util.Objects;
 
+@SuppressWarnings("unchecked")
 public final class Proxys {
-
-    private static volatile Object instance;
 
     private Proxys() {
     }
@@ -13,20 +12,28 @@ public final class Proxys {
     /**
      * 获取代理对象
      *
-     * @see Singleton 此注解用来标记此对象创建单例模式
+     * @param t 实例化对象
      */
-    @SuppressWarnings("unchecked")
+    public static <T> T getInstance(T t) {
+        var loader = t.getClass().getClassLoader();
+        var inter = t.getClass().getInterfaces();
+        return (T) Proxy.newProxyInstance(loader, inter, (proxy, method, args) -> method.invoke(t, args));
+    }
+
+    /**
+     * 获取反射代理对象
+     *
+     * @param t 对象类
+     */
     public static <T> T getInstance(Class<T> t) {
         try {
-            Class<?> clazz = Class.forName(Objects.requireNonNull(t.getCanonicalName()));
-            if (clazz.isAnnotationPresent(Singleton.class)) {
-                if (instance == null) synchronized (Class.class) {
-                    if (instance == null) instance = newInstance(clazz);
-                }
-            } else instance = newInstance(clazz);
-            return (T) instance;
+            var clazz = Class.forName(Objects.requireNonNull(t.getCanonicalName()));
+            T instance = (T) newInstance(clazz);
+            synchronized (Class.class) {
+                return instance;
+            }
         } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
+            System.out.println(Proxys.class.getName() + "-异常: " + e.getMessage());
             return null;
         }
     }
@@ -35,9 +42,9 @@ public final class Proxys {
      * 根据反射实例化代理对象
      */
     private static <T> Object newInstance(Class<T> clazz) throws Exception {
-        T newInstance = clazz.getDeclaredConstructor().newInstance();
-        ClassLoader loader = newInstance.getClass().getClassLoader();
-        Class<?>[] inter = newInstance.getClass().getInterfaces();
+        var newInstance = (T) clazz.getDeclaredConstructor().newInstance();
+        var loader = newInstance.getClass().getClassLoader();
+        var inter = newInstance.getClass().getInterfaces();
         return Proxy.newProxyInstance(loader, inter, (proxy, method, args) -> method.invoke(newInstance, args));
     }
 
